87c1df46eca5ea84e5b62d81a01ece8f70f16a6b
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-07 23:40:24 +0900

    Day30 - Made it read the CSV(text) files and return temperatures for any station and date.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+import pandas as pd
 
 # Create a website object:
 app = Flask(__name__)
 
-
+# df = pd.read_csv()
 # Refer to the app variable. Use route() that belongs to the app object:
 @app.route("/")
 def home():
@@ -12,8 +13,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    # df = pandas.read_scv("")
-    temperature = 23  # df.station(data)
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {
         "station": station,
         "date": date,
@@ -23,3 +25,5 @@ def about(station, date):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
